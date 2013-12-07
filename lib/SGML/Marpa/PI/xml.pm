a3127a6a443c747d0a6077b8eef0a36036a7620e
Paul Bennett <paul.w.bennett@gmail.com>
2013-12-07 10:16:38 -0500

    what?

--- lib/SGML/Marpa/PI/xml.pm
+++ lib/SGML/Marpa/PI/xml.pm
@@ -4,30 +4,9 @@ use base qw( SGML::Marpa );
 
 sub make {
     my $self = shift;
-    $self->{ fragments }->{ Prolog } = xml_prolog();
-    my $g = $self->{ fragments }->{ DefaultG0 };
-    $g =~ s{^netc ~ '/'$}{netc ~ '>'}smg;
-    $self->{ fragments }->{ DefaultG0 } = $g;
+    $self->{ fragments }->{ Prolog } =~ s/Prolog ::= OPList DTD DTDOP LTDOP/Prolog ::= OPList DTDOP LTDOP/smg;
+    $self->{ fragments }->{ L0 } =~ s{^netc ~ '/'$}{netc ~ '>'}smg;
     return $self->SUPER::make;
-}
-
-sub xml_prolog {
-    # remove the "pure SGML" requirement for at least one DTD
-    # NB: DTDs (if any) still have to come before LTDs (if any)
-    return << 'MARPA';
-Prolog ::= OPList DTDOP LTDOP
-DTDOP ::= DTD DTDOP
-        | OtherProlog DTDOP
-        | Nil
-LTDOP ::= LTD LTDOP
-        | OtherProlog LTDOP
-        | Nil
-OPList ::= OtherProlog+ separator => s proper => 1
-OtherProlog ::= CommentDeclaration
-              | ProcessingInstruction
-              | s
-              | Nil
-MARPA
 }
 
 1;
